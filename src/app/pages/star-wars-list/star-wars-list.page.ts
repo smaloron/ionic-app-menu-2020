435e870871c24e04a510ad09d5e6e3f319b4340e
Sebastien Maloron <contact@smaloron.eu>
2020-12-09 12:10:25 +0100

    Liens et suivant et précédent

--- src/app/pages/star-wars-list/star-wars-list.page.ts
+++ src/app/pages/star-wars-list/star-wars-list.page.ts
@@ -15,25 +15,42 @@ export class StarWarsListPage implements OnInit {
   // Tableau qui va accueillir les données
   public characterList = [];
 
+  public nextURL: string;
+  public prevURL: string;
+  public page = 1;
+
   // Injection d'une instance de HttpClient dans le constructeur
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     // Appel de la fonction chargée de récupérer les données depuis l'API
-    this.loadData();
+    this.loadData(URL);
   }
 
   /**
    * Fonction de récupération des données via une requête HTTP
+   * En passant l'URL en argument
    */
-  private loadData() {
-    this.http.get(URL)
+  private loadData(dataUrl) {
+    this.http.get(dataUrl)
       .subscribe(
         (response: any) => {
           console.log(response);
           this.characterList = response.results;
+          this.nextURL = response.next;
+          this.prevURL = response.previous;
         }
       );
+  }
+
+  public nextPage() {
+    this.loadData(this.nextURL);
+    this.page++;
+  }
+
+  public previousPage() {
+    this.loadData(this.prevURL);
+    this.page--;
   }
 
 }
